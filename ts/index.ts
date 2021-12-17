700296306d4c848a99e4436e745fe0a33c0de275
0324bence <20050324bence@gmail.com>
2021-12-17 22:54:38 +0100

    smooth

--- ts/index.ts
+++ ts/index.ts
@@ -5,6 +5,7 @@ class Settings {
     public static SPEED = 5;
     public static SIZE: numTuple = [40, 40];
     public static MOVESIZE = 40;
+    public static PIXELS_PER_FRAME = Math.floor(Settings.SIZE[0] / Settings.SPEED);
     public static SNAKE_COLOUR = "black";
     public static SNAKE_HEAD = "blue";
     public static FOOD_COLOUR = "red";
@@ -23,13 +24,13 @@ class Food extends Settings {
     }
 
     public pos: numTuple = [
-        Math.floor(Math.random() * (this.slots[0] - 1) + 1) * Settings.SIZE[0],
-        Math.floor(Math.random() * (this.slots[1] - 1) + 1) * Settings.SIZE[1],
+        Math.floor(Math.random() * (this.slots[0] - 1) + 1) * Settings.SIZE[0] + Settings.SIZE[0] / 2,
+        Math.floor(Math.random() * (this.slots[1] - 1) + 1) * Settings.SIZE[1] + Settings.SIZE[1] / 2,
     ];
 
     New(pos: numTuple, snake: numTuple[]) {
-        const a = Math.floor(Math.random() * (this.slots[0] - 1) + 1) * Settings.SIZE[0];
-        const b = Math.floor(Math.random() * (this.slots[1] - 1) + 1) * Settings.SIZE[1];
+        const a = Math.floor(Math.random() * (this.slots[0] - 1) + 1) * Settings.SIZE[0] + Settings.SIZE[0] / 2;
+        const b = Math.floor(Math.random() * (this.slots[1] - 1) + 1) * Settings.SIZE[1] + Settings.SIZE[1] / 2;
         if (pos[0] != a && pos[1] != b) {
             for (let i in snake) {
                 if (snake[i][0] == a && snake[i][1] == b) {
@@ -46,7 +47,9 @@ class Food extends Settings {
 
         this.ctx.beginPath();
         this.ctx.fillStyle = Settings.FOOD_COLOUR;
-        this.ctx.fillRect(...this.pos, ...Settings.SIZE);
+        this.ctx.lineWidth = 0;
+        this.ctx.arc(...this.pos, Settings.SIZE[0] / 2, 0, 2 * Math.PI, false);
+        this.ctx.fill();
         this.ctx.closePath();
     }
 }
@@ -58,13 +61,98 @@ class Snake extends Settings {
 
     private Velocity: [-1 | 0 | 1, -1 | 0 | 1] = [0, 0];
     private pos: numTuple = [
-        Math.floor(Math.random() * (this.slots[0] - 1) + 1) * Settings.SIZE[0],
-        Math.floor(Math.random() * (this.slots[1] - 1) + 1) * Settings.SIZE[1],
+        Math.floor(Math.random() * (this.slots[0] - 1) + 1) * Settings.SIZE[0] + Settings.SIZE[0] / 2,
+        Math.floor(Math.random() * (this.slots[1] - 1) + 1) * Settings.SIZE[1] + Settings.SIZE[1] / 2,
     ];
     private snake: numTuple[] = [];
+    private moves: string[] = [];
 
     Input = (event: KeyboardEvent) => {
         switch (event.key) {
+            case "ArrowUp":
+                this.moves.push(event.key);
+                break;
+            case "ArrowDown":
+                this.moves.push(event.key);
+                break;
+            case "ArrowRight":
+                this.moves.push(event.key);
+                break;
+            case "ArrowLeft":
+                this.moves.push(event.key);
+                break;
+        }
+    };
+
+    CollisionCheck() {
+        if (
+            this.pos[0] <= 0 ||
+            this.pos[1] <= 0 ||
+            this.pos[0] > this.PLAYAREA[0] - Settings.SIZE[0] / 2 ||
+            this.pos[1] > this.PLAYAREA[1] - Settings.SIZE[1] / 2
+        ) {
+            gameOver = true;
+            return;
+        }
+
+        if (this.pos[0] == this.food.pos[0] && this.pos[1] == this.food.pos[1]) {
+            this.snake.push(this.pos);
+            this.food.New(this.pos, this.snake);
+            return;
+        }
+
+        for (let i in this.snake) {
+            if (this.pos[0] == this.snake[i][0] && this.pos[1] == this.snake[i][1]) {
+                gameOver = true;
+                return;
+            }
+        }
+    }
+
+    private movement = 0;
+
+    Draw() {
+        if (gameOver) {
+            this.ctx.beginPath();
+            this.ctx.font = `60px Arial`;
+            this.ctx.textAlign = "center";
+            this.ctx.textBaseline = "middle";
+            this.ctx.fillStyle = "black";
+            this.ctx.fillText("Game Over", this.PLAYAREA[0] / 2, this.PLAYAREA[1] / 2);
+            this.ctx.font = `30px Arial`;
+            this.ctx.fillText("Press ENTER to restart", this.PLAYAREA[0] / 2, this.PLAYAREA[1] / 2 + 50);
+            this.ctx.closePath();
+            return;
+        }
+
+        this.ctx.beginPath();
+        this.ctx.fillStyle = Settings.SNAKE_HEAD;
+        this.ctx.arc(...this.pos, Settings.SIZE[0] / 2, 0, 2 * Math.PI, false);
+        this.ctx.fill();
+        this.ctx.closePath();
+
+        for (let i in this.snake) {
+            this.ctx.beginPath();
+            this.ctx.fillStyle = Settings.SNAKE_COLOUR;
+            this.ctx.arc(...this.snake[i], Settings.SIZE[0] / 2, 0, 2 * Math.PI, false);
+            this.ctx.fill();
+            this.ctx.closePath();
+        }
+
+        this.snake = this.snake.map((val, idx, arr) => {
+            if (idx === 0) return this.pos;
+            return arr[idx - 1];
+        });
+
+        this.pos = this.pos.map((num, i) => {
+            return num + this.Velocity[i] * Settings.PIXELS_PER_FRAME;
+        }) as numTuple;
+
+        this.movement++;
+
+        if (this.movement !== Settings.SPEED) return;
+
+        switch (this.moves[0]) {
             case "ArrowUp":
                 if (this.Velocity[1] == 0) {
                     this.Velocity[1] = -1;
@@ -90,73 +178,12 @@ class Snake extends Settings {
                 }
                 break;
         }
-    };
 
-    CollisionCheck() {
-        if (this.pos[0] == this.food.pos[0] && this.pos[1] == this.food.pos[1]) {
-            this.snake.push(this.pos);
-            this.food.New(this.pos, this.snake);
-            return;
-        }
-
-        for (let i in this.snake) {
-            if (this.pos[0] == this.snake[i][0] && this.pos[1] == this.snake[i][1]) {
-                gameOver = true;
-                return;
-            }
-        }
-
-        console.log("collision");
-    }
-
-    private movement = 0;
-
-    Draw() {
-        if (gameOver) {
-            this.ctx.beginPath();
-            this.ctx.font = `60px Arial`;
-            this.ctx.textAlign = "center";
-            this.ctx.textBaseline = "middle";
-            this.ctx.fillStyle = "black";
-            this.ctx.fillText("Game Over", this.PLAYAREA[0] / 2, this.PLAYAREA[1] / 2);
-            this.ctx.font = `30px Arial`;
-            this.ctx.fillText("Press ENTER to restart", this.PLAYAREA[0] / 2, this.PLAYAREA[1] / 2 + 50);
-            this.ctx.closePath();
-            return;
-        }
-
-        this.ctx.beginPath();
-        this.ctx.fillStyle = Settings.SNAKE_HEAD;
-        this.ctx.fillRect(...this.pos, ...Settings.SIZE);
-        this.ctx.closePath();
-
-        for (let i in this.snake) {
-            this.ctx.beginPath();
-            this.ctx.fillStyle = Settings.SNAKE_COLOUR;
-            this.ctx.fillRect(...this.snake[i], ...Settings.SIZE);
-            this.ctx.closePath();
-        }
-
-        if (
-            this.pos[0] <= 0 ||
-            this.pos[1] <= 0 ||
-            this.pos[0] >= this.PLAYAREA[0] ||
-            this.pos[1] >= this.PLAYAREA[1]
-        ) {
-            gameOver = true;
-            return;
-        }
+        this.moves.shift();
 
         this.CollisionCheck();
 
-        this.snake = this.snake.map((val, idx, arr) => {
-            if (idx === 0) return this.pos;
-            return arr[idx - 1];
-        });
-
-        this.pos = this.pos.map((num, i) => {
-            return num + this.Velocity[i] * Settings.SIZE[i];
-        }) as numTuple;
+        this.movement = 0;
     }
 }
 
